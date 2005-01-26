b1c0b3300f8bed6d765176017059c1bb149b7c96
Charles J. Geyer <charlie@stat.umn.edu>
2005-01-26 00:00:00 +0000

    version 0.3

--- src/get_my_priority.c
+++ src/get_my_priority.c
@@ -29,30 +29,25 @@
 * copyright holder.
 */
 
-#ifndef Win32
+#include <sys/time.h>
 #include <sys/resource.h>
 #include <errno.h>
 #include <string.h>
-#endif
 #include <R.h>
 
 void
 get_my_priority(int *result)
 {
-#ifndef Win32
     errno = 0;
     result[0] = getpriority(PRIO_PROCESS, 0);
     if (errno != 0)
         warning(strerror(errno));
-#endif
 }
 
 void
 set_my_priority(int *priority)
 {
-#ifndef Win32
     if (setpriority(PRIO_PROCESS, 0, priority[0]) != 0)
         warning(strerror(errno));
-#endif
 }
 
